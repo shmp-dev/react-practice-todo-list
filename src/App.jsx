c1e02a5165f164d0aca6d6c22dbc89c9339a36ff
shmp-dev <128882665+shmp-dev@users.noreply.github.com>
2023-05-23 13:40:40 +0000

    [fix]ステートを定義

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  // TODOテキストのステート
+  const [todoText, setTodoText] = useState("初期");
+  // 未完了TODOリストの配列ステート
+  const [incompleteList, setIncompleteList] = useState(["aaaa", "iiii"]);
+  // 完了TODOリストの配列ステート
+  const [completeList, setCompleteList] = useState(["uuuu"]);
+
+  // TODOテキストインプットの内容をステートに更新
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  // 追加ボタン押下時
+  const onClickAddButton = () => {
+    alert("追加");
+  };
+
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAddButton}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          <div className="list-row">
-            <li>aaaaa</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
-          <div className="list-row">
-            <li>iiiii</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
+          {incompleteList.map((todo, index) => {
+            return (
+              <div key={index} className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
         </ul>
       </div>
       <div className="complete-area">
         <p className="title">完了のTODO</p>
         <ul>
-          <div className="list-row">
-            <li>uuuuu</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
+          {completeList.map((todo, index) => {
+            return (
+              <div key={index} className="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
         </ul>
       </div>
     </>
